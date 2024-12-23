afd58af6d13b8805a54d55654e63fe264cefda30
Name <email@example.ru>
2024-12-23 12:45:04 +0300

    Задание 3.3.2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,\n World!");
+//Console.WriteLine("Hello,\n World!");
 
 string MyName;
 MyName = "Pavel";
+byte age = 39;
+bool havePet = true;
+float shoeSize = 43.5f;
 
-Console.WriteLine(MyName);
+//Console.WriteLine(MyName);
 
-Console.WriteLine("\t Мне 39 лет");
-Console.WriteLine("\t My name is \n {0}", MyName);
-Console.WriteLine("\u0040");
+//Console.WriteLine("\t Мне 39 лет");
+//Console.WriteLine("\t My name is \n {0}", MyName);
+//Console.WriteLine("\u0040");
 
-Console.ReadKey();
+//Console.ReadKey();
+
+Console.WriteLine("My name is " + MyName);
+Console.WriteLine("My age is " + age);
+Console.WriteLine("Do you have a pet?" + havePet);
+Console.WriteLine("My shoe size " + shoeSize);
