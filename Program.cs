1e0eaa6ddfff54353a95bac49716150656ceac27
Name <email@example.ru>
2024-12-23 12:33:07 +0300

    Работа с символами переноса, табуляции и спеециальные символы.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello,\n World!");
 
 string MyName;
-
 MyName = "Pavel";
 
 Console.WriteLine(MyName);
+
+Console.WriteLine("\t Мне 39 лет");
+Console.WriteLine("\t My name is \n {0}", MyName);
+Console.WriteLine("\u0040");
 
 Console.ReadKey();
